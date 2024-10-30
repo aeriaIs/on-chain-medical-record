40e111ae460f13fa9617d182488431d095244e89
aerials <nosvengeance@gmail.com>
2024-10-30 22:52:01 +0700

    tailwind: remove unused custom, fix content source & custom extend color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,38 @@
-'use client'
+"use client";
 
-import {Auth} from '@/components/auth'
-import {Background} from '@/components/background'
-import {Footer} from '@/components/footer'
-import {Modal} from '@/components/modal'
-import {Table} from '@/components/table'
-import {initSatellite} from '@junobuild/core-peer'
-import {useEffect} from 'react'
+import { useEffect } from "react";
+import { initSatellite } from "@junobuild/core-peer";
 
-export default function Home () {
+import AuthLayout from "@/ui/layout/AuthLayout";
+import { Footer } from "@/components/footer";
+import { Modal } from "@/components/modal";
+import { Table } from "@/components/table";
+
+export default function Home() {
   useEffect(() => {
     (async () =>
       await initSatellite({
         workers: {
           auth: true
         }
-      }))()
-  }, [])
+      }))();
+  }, []);
 
   return (
     <>
       <div className="relative isolate min-h-[100dvh]">
-        <main className="mx-auto max-w-screen-2xl py-16 px-8 md:px-24 tall:min-h-[calc(100dvh-128px)]">
-          <h1 className="dark:text-white text-5xl md:text-6xl font-bold tracking-tight md:pt-24">
-            Example App
-          </h1>
-          <p className="dark:text-white py-4 md:max-w-lg">
-            Explore this demo app built with Next.js, Tailwind, and{' '}
-            <a
-              href="https://juno.build"
-              rel="noopener noreferrer"
-              target="_blank"
-              className="underline">
-              Juno
-            </a>
-            , showcasing a practical application of these technologies.
-          </p>
+        <main className="mx-auto max-w-screen-2xl py-16 px-8 md:px-24">
+          <h1 className="text-5xl md:text-6xl font-bold tracking-tight md:pt-24">Medical Tracker</h1>
 
-          <Auth>
+          <AuthLayout>
             <Table />
 
             <Modal />
-          </Auth>
+          </AuthLayout>
         </main>
 
         <Footer />
-
-        <Background />
       </div>
     </>
-  )
+  );
 }
---
+++ src/components/core/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full h-14 py-2 px-4 lg:px-6 flex flex-row justify-between items-center border-y border-neutral-500">
+      <div>
+        asd
+      </div>
+    </nav>
+  );
+}
